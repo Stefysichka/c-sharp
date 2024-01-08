13941c25bdb3c5a903a104d058f51e51dfa3b4da
Stefysichka <srefaniamavdruk@gmail.com>
2024-01-08 23:10:39 +0200

    Add project files.

---
+++ trainings_/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace trainings_
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}");
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Person person = new Person();
+            person.Name = "John";
+            person.Age = 30;
+            person.PrintInfo();
+        }
+    }
+}
